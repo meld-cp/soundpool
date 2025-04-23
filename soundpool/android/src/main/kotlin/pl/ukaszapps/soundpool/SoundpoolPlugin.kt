add6badc11a49e016028abddb583a305f3d24c7c
Cleon Pinto <>
2025-04-23 20:07:45 +1200

    Remove deprecated registerWith method for plugin registration

--- soundpool/android/src/main/kotlin/pl/ukaszapps/soundpool/SoundpoolPlugin.kt
+++ soundpool/android/src/main/kotlin/pl/ukaszapps/soundpool/SoundpoolPlugin.kt
@@ -14,7 +14,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URI
@@ -25,17 +24,9 @@ internal val loadExecutor: Executor = Executors.newCachedThreadPool()
 
 internal val uiThreadHandler: Handler = Handler(Looper.getMainLooper())
 
-class SoundpoolPlugin : MethodCallHandler, FlutterPlugin {
-    companion object {
-        @Suppress("unused")
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            var pool = SoundpoolPlugin()
-            pool.onRegister(registrar.context(), registrar.messenger())
-        }
+private const val CHANNEL_NAME = "pl.ukaszapps/soundpool"
 
-        private const val CHANNEL_NAME = "pl.ukaszapps/soundpool"
-    }
+class SoundpoolPlugin : MethodCallHandler, FlutterPlugin {
 
     private fun onRegister(context: Context,  messenger: BinaryMessenger) {
         application = context.applicationContext
